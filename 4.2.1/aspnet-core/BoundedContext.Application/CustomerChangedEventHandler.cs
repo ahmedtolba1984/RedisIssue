1b4f66d55a39473d78d75f96626c0ab2805f7511
a.tolbah <a.tolbah@alwefaq.com>
2018-12-06 18:17:56 +0200

    Add Event Handler

---
+++ 4.2.1/aspnet-core/BoundedContext.Application/CustomerChangedEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Events.Bus.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Abp.EntityHistory;
+using Abp.Events.Bus.Entities;
+
+namespace BoundedContext.Application
+{
+    public class CustomerChangedEventHandler : IAsyncEventHandler<EntityChangedEventData<Entity<int>>>
+    {
+        public async Task HandleEventAsync(EntityChangedEventData<Entity<int>> eventData)
+        {
+
+        }
+    }
+}
